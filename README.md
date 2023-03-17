4a3a309be8f2b371478348728b2b1288d1a5e8f3
VS Eiman <eltiganieiman@gmail.com>
2023-03-17 08:50:37 -0400

    uploading homework

---
+++ README.md
@@ -0,0 +1,6 @@
+# Tech Talent South Homework
+
+## JUnit with Spring Boot
+### Complete the tutorial found at this url:
+* https://spring.io/guides/gs/testing-web/
+* When you are finished with the (15 minute) assignment attach your GitHub.
---
+++ src/main/java/com/example/demo/WebLayerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@WebMvcTest
+public class WebLayerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+				.andExpect(content().string(containsString("Hello, World")));
+	}
+}
